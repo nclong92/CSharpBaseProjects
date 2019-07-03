b25757153a887b69f9ede6baf18de7004543b693
longnc <nclong92@gmail.com>
2019-07-03 17:17:43 +0700

    String, Array, Dictinary

--- CSharpBaseProjects/Program.cs
+++ CSharpBaseProjects/Program.cs
@@ -1,12 +1,202 @@
 ﻿using System;
+using System.Linq;
+using System.Text;
 
 namespace CSharpBaseProjects
 {
     class Program
     {
+        //Nhập vào 1 chuỗi, sau đó
+        //    1. Đếm xem chuỗi có bao nhiêu in HOA, in thường, số
+        //    2. Đếm số khoảng trắng trong chuỗi
+        static void XuLyChuoi()
+        {
+            Console.WriteLine("Chương trình xử lý chuỗi");
+            Console.WriteLine("Nhập vào 1 chuỗi bất kỳ: ");
+            string chuoiDaNhap = Console.ReadLine();
+            char[] chuoiDaNhapArr = chuoiDaNhap.ToCharArray();
+
+            int soKyTuInHoa = 0;
+            int soKyTuThuong = 0;
+            int soKyTuSo = 0;
+            int soKhoangTrang = 0;
+
+            for (int i = 0; i < chuoiDaNhapArr.Length; i++)
+            {
+                char c = chuoiDaNhapArr[i];
+                if (Char.IsUpper(c))
+                    soKyTuInHoa++;
+
+                if (Char.IsLower(c))
+                    soKyTuThuong++;
+
+                if (Char.IsDigit(c))
+                    soKyTuSo++;
+
+                if (Char.IsWhiteSpace(c))
+                    soKhoangTrang++;
+            }
+
+            Console.WriteLine($"Số ký tự in HOA: {soKyTuInHoa}");
+            Console.WriteLine($"Số ký tự thường: {soKyTuThuong}");
+            Console.WriteLine($"Số ký tự số: {soKyTuSo}");
+            Console.WriteLine($"Số ký tự là khoảng trắng: {soKhoangTrang}");
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.OutputEncoding = Encoding.UTF8;
+            //Console.WriteLine("Hello World!");
+            //XuLyChuoi();
+            //ToiUuChuoi();
+            //Mang1Chieu();
+            Mang2Chieu();
+
+
+            Console.ReadLine();
+        }
+
+        /*
+         * Khai báo, nhập, xuất mảng 2 chiều
+         * 
+         */
+        private static void Mang2Chieu()
+        {
+            Console.WriteLine("Mảng 2 chiều.");
+            Console.Write("\nNhập vào số dòng: ");
+            int soDong = Int32.Parse(Console.ReadLine());
+
+            Console.Write("\nNhập vào số cột: ");
+            int soCot = Int32.Parse(Console.ReadLine());
+
+            int[,] M = new int[soDong, soCot];
+
+            Random random = new Random();
+            for(int i = 0; i < M.GetLength(0); i++)
+            {
+                for(int j = 0; j < M.GetLength(1); j++)
+                {
+                    M[i,j] = random.Next(0, 100);
+                }
+            }
+
+            Console.WriteLine($"\nMảng 2 chiều ngẫu nhiên với {soDong} dòng và {soCot} là: ");
+            for (int i = 0; i < M.GetLength(0); i++)
+            {
+                for (int j = 0; j < M.GetLength(1); j++)
+                {
+                    Console.Write(M[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        /*
+         * Tạo 1 mảng M có n phần tử, sau đó:
+         * 1) Nhập giá trị ngẫu nhiên cho các phần tử trong mảng M
+         * 2) Xuất các giá trị trong mảng
+         * 3) Đảo ngược mảng
+         * 4) Sắp xếp mảng
+         * 5) Tính tổng các phần tử trong mảng
+         * 6) Tìm kiếm mảng
+         */
+        private static void Mang1Chieu()
+        {
+            Console.WriteLine("Tạo mới mảng 1 chiều");
+            Console.WriteLine("Nhập số phần tử của mảng: ");
+            int n = int.Parse(Console.ReadLine());
+            Random random = new Random();
+            int[] M = new int[n];
+
+            Console.WriteLine($"\nMảng M có {n} phần tử ngẫu nhiên là:");
+            for (int i = 0; i < M.Length; i++)
+            {
+                M[i] = random.Next(0, 100);
+                Console.Write($"{M[i]} \t");
+            }
+
+            Console.WriteLine("\n\nMảng M sau khi đảo ngược: ");
+            Array.Reverse<int>(M);
+            for(int i = 0; i < M.Length; i++)
+            {
+                Console.Write($"{M[i]} \t");
+            }
+
+            Console.WriteLine("\n\nMảng sau khi sắp xếp là: ");
+            Array.Sort<int>(M);
+            for (int i = 0; i < M.Length; i++)
+            {
+                Console.Write($"{M[i]} \t");
+            }
+
+            int arrSum = 0;
+            for (int i = 0; i < M.Length; i++)
+            {
+                arrSum += M[i];
+            }
+            Console.WriteLine($"\n\nTổng các phần tử trong mảng là: {arrSum}");
+
+            Console.WriteLine("\n\nNhập vào số muốn tìm kiếm: ");
+            int soCanTim = Int16.Parse(Console.ReadLine());
+
+            /*
+            int kq = Array.BinarySearch(M, soCanTim);
+            if (kq == -1)
+                Console.WriteLine($"Không tìm thấy số {soCanTim} trong mảng");
+            else
+                Console.WriteLine($"Số {soCanTim} được tìm thấy ở vị trí {kq}");
+            */
+            int kq = -1;
+            for(int i = 0; i < M.Length; i++)
+            {
+                if(M[i] == soCanTim) { 
+                    Console.WriteLine($"Số {soCanTim} được tìm thấy ở vị trí {i}");
+                    kq = i;
+                    break;
+                }
+            }
+
+            if(kq == -1)
+                Console.WriteLine($"Không tìm thấy số {soCanTim} trong mảng");
+        }
+
+        /*
+        Cho 1 chuỗi gốc, yêu cầu:
+            Xóa các khoảng trăng dư thừa bên trái, phải chuỗi, các từ cách nhau bằng 1 khoảng trắng,
+            ký tự đầu tiên viết hoa
+            ví dụ: Nguyễn công lonG quân
+            sau khi tối ưu: Nguyễn Công Long Quân
+        */
+        private static void ToiUuChuoi()
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine("Nhập vào 1 tên bất kỳ: ");
+            string strInput = Console.ReadLine();
+            strInput.Trim();
+
+            // Get Array 
+            // arrInput[0]="Nguyễn"
+            // arrInput[1]="công"
+            // arrInput[2]="lonG"
+            // arrInput[3]="quân"
+            string[] arrInput = strInput.Split(
+                new char[] { ' ' },
+                StringSplitOptions.RemoveEmptyEntries);
+
+            string strOutput = "";
+
+            for (int i = 0; i < arrInput.Length; i++)
+            {
+                string word = arrInput[i];
+                word = word.ToLower();
+                char[] wordArr = word.ToCharArray();
+                wordArr[0] = char.ToUpper(wordArr[0]);
+                string newWord = new string(wordArr);
+                strOutput += newWord + " ";
+            }
+
+            Console.WriteLine($"Chuỗi ban đầu là: {strInput}");
+            Console.WriteLine($"Chuỗi kết quả là: {strOutput}");
+
         }
     }
 }
---
+++ TraCuuTuDien/Program.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TraCuuTuDien
+{
+    class Program
+    {
+        static Dictionary<string, string> dic = new Dictionary<string, string>();
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            while (true)
+            {
+                menu();
+
+                Console.WriteLine("Bạn có muốn tiếp tục sử dụng Từ điển không? (c/k)?");
+                string str = Console.ReadLine();
+                if (str.ToLower().Equals("k"))
+                    break;
+            }
+            Console.WriteLine("Ket thuc su dung Tu dien");
+        }
+
+        private static void menu()
+        {
+            Console.WriteLine("1. Nhập từ mới.");
+            Console.WriteLine("2. Sửa từ.");
+            Console.WriteLine("3. Tra cứu.");
+            Console.WriteLine("4. Xóa từ.");
+            Console.WriteLine("Lựa chọn chức năng: ");
+
+            Console.WriteLine();
+
+            int cn = int.Parse(Console.ReadLine());
+            try
+            {
+                switch (cn)
+                {
+                    case 1:
+                        TaoTuMoi();
+                        break;
+                    case 2:
+                        SuaTu();
+                        break;
+                    case 3:
+                        TraTu();
+                        break;
+                    case 4:
+                        XoaTu();
+                        break;
+                    default:
+                        Console.WriteLine("Chức năng không tồn tại");
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Lỗi chương trình. {ex.Message}");
+            }
+
+        }
+
+        private static void XoaTu()
+        {
+            Console.WriteLine("Nhập vào từ muốn xóa: ");
+            string deleteWord = Console.ReadLine().ToLower();
+
+            if (dic.ContainsKey(deleteWord))
+            {
+                Console.WriteLine($"Bạn có chắc chắn muốn xóa từ {deleteWord}? (c/k)?");
+                string str = Console.ReadLine();
+                if (str.ToLower().Equals("c"))
+                    dic.Remove(deleteWord);
+            }
+            else
+            {
+                Console.WriteLine($"Từ [{deleteWord}] không có trong từ điển.");
+            }
+        }
+
+        private static void TraTu()
+        {
+            Console.WriteLine("Nhập vào từ muốn tìm kiếm nghĩa tiếng Việt: ");
+            string searchWord = Console.ReadLine().ToLower();
+
+            if(dic.ContainsKey(searchWord))
+            {
+                Console.WriteLine($"Nghĩa của từ [{searchWord}] là {dic[searchWord]}");
+            } else
+            {
+                Console.WriteLine($"Từ [{searchWord}] chưa được cập nhật trong từ điển.");
+            }
+        }
+
+        private static void SuaTu()
+        {
+            Console.WriteLine("Nhập từ muốn sửa nghĩa: ");
+            string enWord = Console.ReadLine().ToLower();
+
+            if (!dic.ContainsKey(enWord))
+                Console.WriteLine($"Không tìm thấy từ [{enWord}]");
+            else
+            {
+                Console.WriteLine("Nhập nghĩa tiếng Việt: ");
+                string newViWord = Console.ReadLine();
+                dic[enWord] = newViWord;
+            }
+        }
+
+        private static void TaoTuMoi()
+        {
+            Console.WriteLine("Nhập vào từ tiếng Anh: ");
+            string enWord = Console.ReadLine().ToLower();
+
+            if (dic.ContainsKey(enWord))
+                Console.WriteLine($"Từ {enWord} đã tồn tại.");
+            else
+            {
+                Console.WriteLine("Nhập vào nghĩa tiếng Việt");
+                string viWord = Console.ReadLine();
+
+                dic.Add(enWord, viWord);
+            }
+        }
+    }
+}
